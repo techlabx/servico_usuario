9f4c2f982067c0f873966ebae60caba7816928bd
giovanadanieles <giovana.daniele@usp.br>
2020-06-04 20:09:06 -0300

    Inserindo CRUD usuario comum e gapsi

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  development: {
+    client: 'postgres',
+
+    connection:{
+      host: '127.0.0.1',
+      user: 'gds',
+      password: 'admin',
+      database: 'smBD',
+      charset: 'utf8'
+    }
+
+  }
+};
