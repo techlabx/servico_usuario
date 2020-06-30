869c148a2d5a1fb353a9e164c9df129a26271088
Leonardo Prati <leonardo@sflabs.com.br>
2020-06-29 21:14:24 -0300

    Altera dados de conexao com o banco para adequar a execucao via docker

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,9 @@ module.exports = {
     client: 'postgres',
 
     connection:{
-      host: '127.0.0.1',
-      // host: 'http://34.67.221.90:49165',
-      user: 'gds',
-      password: 'admin',
+      host: 'database',
+      user: 'postgres',
+      password: 'techlab',
       database: 'smBD',
       charset: 'utf8'
     }
