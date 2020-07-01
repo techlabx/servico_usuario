86b757074032a5cb20bfe2a756f51373182ad498
Leonardo Prati <leonardo@sflabs.com.br>
2020-07-01 17:10:19 -0300

    Altera sintaxe do module exports nas queries. Altera funcoes bindadas nas rotas para ser async

--- api/usuario.js
+++ api/usuario.js
@@ -28,11 +28,10 @@ function validAtend(usuario){
 
 // ------------------------------------------------------------------- Usuário GAPsi/Apoia
 
-router.get('/gapsi/', (req, res) => {
+router.get('/gapsi', async (req, res) => {
     try {
-        queries.getAllAtend().then(usuario => {
-            res.json(usuario);
-        });
+        let usuarios = await queries.getAllAtend();
+        res.status(200).json(usuarios);
     }
     catch (err) {
         console.error(err);
@@ -40,42 +39,48 @@ router.get('/gapsi/', (req, res) => {
     }
 });
 
-router.post('/gapsi/', (req, res, next) => {
-    if(validAtend(req.body)){
-        //Insert into DB
-        queries.createAtend(req.body).then(usuarios => {
-            res.json(usuarios[0]);
-        });
+router.post('/gapsi', async (req, res, next) => {
+    try {
+        if(validAtend(req.body)){
+            //Insert into DB
+            let result = await queries.createAtend(req.body);
+            res.status(201).send(result[0]);
+        }
+        else next(new Error('Atendente GAPSI/Apoia inválido.'));
     }
-    else next(new Error('Atendente GAPSI/Apoia inválido.'));
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Erro");
+    }
 });
 
-router.put('/gapsi/:emailatendente', (req, res, next) => {
+router.put('/gapsi/:emailatendente', async (req, res, next) => {
     if(validAtend(req.body)){
         //Update the quest
-        queries.updateAtend(req.params.emailatendente, req.body).then(usuarios => {
-            res.json(usuarios[0]);
-        });
+        let usuario = await queries.updateAtend(req.params.emailatendente, req.body)[0];
+        res.status(200).send(usuario)
     }
     else next(new Error('Atendente GAPSI/Apoia inválido. Impossível atualizar.'));
 });
 
-router.delete('/gapsi/:emailatendente', (req, res) => {
-    queries.deleteAtend(req.params.emailatendente).then(() => {
-        res.json({
-            deleted: true
-        });
-    });
+router.delete('/gapsi/:emailatendente', async (req, res) => {
+    try {
+        await queries.deleteAtend(req.params.emailatendente);
+        res.status(200).send({deleted: true});
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Erro");
+    }
 });
 
 
 // ------------------------------------------------------------------- Usuário comum
 
-router.get('/aluno', (req, res) => {
+router.get('/aluno', async (req, res) => {
     try {
-            queries.getAllUsuario().then(usuario => {
-            res.json(usuario);
-        });
+            let usuario = await queries.getAllUsuario();
+            res.status(200).send(usuario);
     }
     catch (err) {
         console.error(err);
@@ -83,60 +88,76 @@ router.get('/aluno', (req, res) => {
     }
 });
 
-router.get('/aluno/:idusuario', (req, res) => {
-    queries.getOneUsuario(req.params.idusuario).then(usuario => {
+router.get('/aluno/:idusuario', async (req, res) => {
+    try {
+        let usuario = await queries.getOneUsuario(req.params.idusuario)
         if(usuario) res.json(usuario);
-        else res.json({error: true});
-    })
+        else res.status(404).send("Not found");
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Error");
+    }
 });
 
 router.post('/aluno', async (req, res, next) => {
-    //Giovana, inserir esses dados abaixo na criação de um usuário
+    try {
+        //Giovana, inserir esses dados abaixo na criação de um usuário
+        let usuario = {
+            userId: req.body.userId,
+            nusp: req.body.nusp,
+            userName: req.body.userName,
+            instituto: req.body.instituto,
+            email: req.body.userEmail
+        }
 
-    let usuario = {
-        userId: req.body.userId,
-        nusp: req.body.nusp,
-        userName: req.body.userName,
-        instituto: req.body.instituto,
-        email: req.body.userEmail
+        if(validUsuario(usuario.userId, usuario.nusp, usuario.userName, usuario.instituto, usuario.email)){
+            //Insert into DB
+            let usuarios = await queries.createUsuario(usuario.userId, usuario.nusp, usuario.userName, usuario.instituto, usuario.email);
+            res.status(200).send(usuarios[0]);
+        }
+        else next(new Error('Usuário inválido.'));
     }
-
-    if(validUsuario(usuario.userId, usuario.nusp, usuario.userName, usuario.instituto, usuario.email)){
-        //Insert into DB
-        queries.createUsuario(usuario.userId, usuario.nusp, usuario.userName, usuario.instituto, usuario.email).then(usuarios => {
-            res.json(usuarios[0]);
-        });
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Error");
     }
-    else next(new Error('Usuário inválido.'));
 });
 
-router.put('/aluno/:idusuario', (req, res, next) => {
-    
-    let usuario = {
-        idusuario: req.body.userId,
-        nuspusuario: req.body.nusp,
-        nomeusuario: req.body.userName,
-        institutousuario: req.body.instituto,
-        emailusuario: req.body.userEmail
-    }
+router.put('/aluno/:idusuario', async (req, res, next) => {
+    try {
+        let usuario = {
+            idusuario: req.body.userId,
+            nuspusuario: req.body.nusp,
+            nomeusuario: req.body.userName,
+            institutousuario: req.body.instituto,
+            emailusuario: req.body.userEmail
+        }
 
-    console.log(usuario);
+        console.log(usuario);
 
-    if(validUsuario(usuario.idusuario, usuario.nuspusuario, usuario.nomeusuario, usuario.institutousuario, usuario.emailusuario)){
-        //Update the quest
-        queries.updateUsuario(req.params.idusuario, usuario).then(usuarios => {
-            res.json(usuarios[0]);
-        });
+        if(validUsuario(usuario.idusuario, usuario.nuspusuario, usuario.nomeusuario, usuario.institutousuario, usuario.emailusuario)){
+            //Update the quest
+            let usuarios = await queries.updateUsuario(req.params.idusuario, usuario);
+            res.status(200).send(usuarios[0]);
+        }
+        else next(new Error('Usuário inválido. Impossível atualizar.'));
     }
-    else next(new Error('Usuário inválido. Impossível atualizar.'));
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Erro");
+    }
 });
 
-router.delete('/aluno/:idusuario', (req, res) => {
-    queries.deleteUsuario(req.params.idusuario).then(() => {
-        res.json({
-            deleted: true
-        });
-    });
+router.delete('/aluno/:idusuario', async (req, res) => {
+    try {
+        await queries.deleteUsuario(req.params.idusuario);
+        res.status(200).send({deleted: true});
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("error");
+    }
 });
 
 
--- db/queries.js
+++ db/queries.js
@@ -1,63 +1,70 @@
 const knex = require('./knex'); // Conexão
 
-module.exports = {
+// --------------------------------------------------------- Usuário GAPsi/Apoia
+// SELECT * FROM atendente;
+async function getAllAtend(){
+    return await knex('atendente');
+}
 
-    // --------------------------------------------------------- Usuário GAPsi/Apoia
+// INSERT INTO atendente VALUES (..., ...);
+async function createAtend(usuario){
+    return await knex('atendente').insert(usuario, '*');
+}
 
-    // SELECT * FROM atendente;
-    async getAllAtend(){
-        return await knex('atendente');
-    },
+// UPDATE atendente SET nomeatendente = '...', emailatendente = '...' WHERE emailatendente = '...';
+async function updateAtend(emailatendente, usuario){
+    return await knex('atendente').where('emailatendente', emailatendente).update(usuario, '*');
+}
 
-    // INSERT INTO atendente VALUES (..., ...);
-    async createAtend(usuario){
-        return await knex('atendente').insert(usuario, '*');
-    },
-
-    // UPDATE atendente SET nomeatendente = '...', emailatendente = '...' WHERE emailatendente = '...';
-    async updateAtend(emailatendente, usuario){
-        return await knex('atendente').where('emailatendente', emailatendente).update(usuario, '*');
-    },
-
-    //DELETE FROM atendente WHERE emailatendente = '...';
-    async deleteAtend(emailatendente){
-        return await knex('atendente').where('emailatendente', emailatendente).del();
-    },
+//DELETE FROM atendente WHERE emailatendente = '...';
+async function deleteAtend(emailatendente){
+    return await knex('atendente').where('emailatendente', emailatendente).del();
+}
 
 
-    // --------------------------------------------------------- Usuário comum
+// --------------------------------------------------------- Usuário comum
 
-    // SELECT * FROM usuario;
-    async getAllUsuario(){
-        return await knex('usuario');
-    },
+// SELECT * FROM usuario;
+async function getAllUsuario(){
+    return await knex('usuario');
+}
 
-    // SELECT * FROM usuario WHERE idusuario = "...";
-    async getOneUsuario(idusuario){
-        return await knex('usuario').where('idusuario', idusuario).first();
-    },
+// SELECT * FROM usuario WHERE idusuario = "...";
+async function getOneUsuario(idusuario){
+    return await knex('usuario').where('idusuario', idusuario).first();
+}
 
-    // INSERT INTO atendente VALUES (..., ...);
-    async createUsuario(idusuario, nusp, nomeusuario, institutousuario, email){
-        return await knex('usuario').insert(
-            {
-                idusuario: idusuario,
-                nuspusuario: nusp,
-                nomeusuario: nomeusuario,
-                institutousuario: institutousuario,
-                emailusuario: email 
-            }
-        );
-    },
+// INSERT INTO atendente VALUES (..., ...);
+async function createUsuario(idusuario, nusp, nomeusuario, institutousuario, email){
+    return await knex('usuario').insert(
+        {
+            idusuario: idusuario,
+            nuspusuario: nusp,
+            nomeusuario: nomeusuario,
+            institutousuario: institutousuario,
+            emailusuario: email 
+        }
+    );
+}
 
-    // UPDATE usuario SET idusuario = '...', nomeusuario = '...' WHERE idusuario = '...';
-    async updateUsuario(idusuario, usuario){
-        return await knex('usuario').where('idusuario', idusuario).update(usuario, '*');
-    },
+// UPDATE usuario SET idusuario = '...', nomeusuario = '...' WHERE idusuario = '...';
+async function updateUsuario(idusuario, usuario){
+    return await knex('usuario').where('idusuario', idusuario).update(usuario, '*');
+}
 
-    //DELETE FROM usuario WHERE idusuario = '...';
-    async deleteUsuario(idusuario){
-        return await knex('usuario').where('idusuario', idusuario).del();
-    }
+//DELETE FROM usuario WHERE idusuario = '...';
+async function deleteUsuario(idusuario){
+    return await knex('usuario').where('idusuario', idusuario).del();
+}
 
+module.exports  = {
+    getAllAtend,
+    createAtend,
+    updateAtend,
+    deleteAtend,
+    getAllUsuario,
+    getOneUsuario,
+    createUsuario,
+    updateUsuario,
+    deleteUsuario
 }
