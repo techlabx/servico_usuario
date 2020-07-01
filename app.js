a1f97e8349d873ab17703280e770650947285777
Leonardo Prati <leonardo@sflabs.com.br>
2020-07-01 16:39:58 -0300

    Adiciona cors e altera a rota base do servico para compatibilidade com o gateway

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -8,13 +9,21 @@ const app = express();
 
 const usuario = require('./api/usuario');
 
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true)
+  }
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 //Mounting the route
-app.use('/usuarios', usuario);
+app.use('/', usuario);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
