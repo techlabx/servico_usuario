91be948b533533dc99112993c10a5362f78aa453
Leonardo Prati <leonardo@sflabs.com.br>
2020-07-01 17:21:22 -0300

    Remove redirecionamento de erro para 404

--- app.js
+++ app.js
@@ -25,18 +25,4 @@ app.use(cookieParser());
 //Mounting the route
 app.use('/', usuario);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
-  });
-});
-
 module.exports = app;
